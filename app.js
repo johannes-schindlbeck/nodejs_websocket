56ea3825e7a8b9efb3dcde8f350d78c64a81fee7
josch <joschindlbeck@googlemail.com>
2024-05-19 18:17:05 +0200

    it works

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const e = require('express');
 const express = require('express');
 
 const app = express();
@@ -8,6 +9,7 @@ const expressWs = require('express-ws')(app);
 const basicAuth = require('express-basic-auth');
 
 var globalWs = null;
+var globalResponse = "";
 
 // Basic Auth
 /**
@@ -18,24 +20,43 @@ app.use(basicAuth({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  if (globalWs) {
-    globalWs.send('Hello, world!');
-  }
-  res.status(200).send('Hello, world!').end();
+  res.status(200).send('Hello, world2!').end();
 });
 
 // API (Post)
 app.post('/api', (req, res) => {
   if (globalWs) {
     console.log("Sending... " + JSON.stringify(req.body));
+    globalResponse = "";
     globalWs.send(JSON.stringify(req.body));
+
+    var timeout = 0;
+    while (globalResponse == "" && timeout < 300) {
+      require('deasync').sleep(100);
+      timeout++;
+    }
+    if (globalResponse == "") {
+      res.json({ error: 'Timeout' });
+    } else {
+      res.json({ response: globalResponse });
+    }
+  } else {
+    res.json({ error: 'No websocket connection' });
   }
-  res.json(req.body);
+});
+
+// API (Response via Websocket)
+app.ws('/wsapi', function (ws, req) {
+  globalWs = ws;
+  ws.on('message', function (msg) {
+    console.log(msg);
+    globalResponse = msg;
+  });
+  console.log('socket', req.testing);
 });
 
 // Websocket
 app.ws('/echo', function (ws, req) {
-  globalWs = ws;
   ws.on('message', function (msg) {
     console.log(msg);
     ws.send(msg);
